1a1ff838ffb038e3a0172700fe9316861d41dbb1
Stefan Olivier <stefan.olivier.car@gmail.com>
2021-01-15 20:36:42 +0200

    Added variable speed and fixed rotation

---
+++ Assets/FollowRocket.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowRocket : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Transform rocketTransform;
+    void Start()
+    {
+        var gameObjectShip = GameObject.Find("3Side Rocket Ship Variant");
+        rocketTransform = gameObjectShip.transform;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FollowRocket_X();
+
+    }
+
+    void FollowRocket_X()
+    {
+        var magnitudeDifference_x = Mathf.Abs(rocketTransform.position.x - transform.position.x);
+        if (magnitudeDifference_x > 4)
+        {
+            if (rocketTransform.position.x > transform.position.x)
+            {
+                transform.Translate(Vector3.right * Time.deltaTime * magnitudeDifference_x);
+            }
+            else
+            {
+                transform.Translate(Vector3.left * Time.deltaTime * magnitudeDifference_x);
+            }
+        }
+    }
+}
--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -2,14 +2,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Rocket : MonoBehaviour
 {
+    [SerializeField] float rcsThrust = 10f;
+    [SerializeField] const float boostThrust = 60f;
+    [SerializeField] float rcsThrustIncrease = 45f;
+    [SerializeField] const float maxBoostVelocity = 10f;   
     Rigidbody rigidBody;
+    AudioSource thrusterSound;
+    float rcsExtraThrust = 0f;
+    float thrustExtraForce = 0f;
+    float thrustExtraForceX = 0f;
     // Start is called before the first frame update
     void Start()
     {
         rigidBody = GetComponent<Rigidbody>();
+        thrusterSound = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -20,17 +30,104 @@ public class Rocket : MonoBehaviour
 
     private void ProcessInput()
     {
-        if (Input.GetKey(KeyCode.W))
+        Thrust();
+        Rotate();
+        ResetRotation();
+    }
+
+    float Max(params float[] values)
+    {
+        float maxValue = 0f;
+        foreach (var item in values)
         {
-            rigidBody.AddRelativeForce(Vector3.up);
+            if (item > maxValue) maxValue = item;
         }
+        return maxValue;
+    }
+
+    bool sameSign(float num1, float num2)
+    {
+        return num1 >= 0 && num2 >= 0 || num1 < 0 && num2 < 0;
+    }
+
+    private void Thrust()
+    {
+        //print(rigidBody.velocity);
+        
+        var maxDirectionalVelocity = Max(rigidBody.GetRelativePointVelocity(Vector3.zero).y);
+        var maxDirectionalVelocityX = Max(rigidBody.GetRelativePointVelocity(Vector3.zero).x, -rigidBody.GetRelativePointVelocity(Vector3.zero).x);
+        //print(maxDirectionalVelocityX + "<->" + rigidBody.GetRelativePointVelocity(Vector3.zero).x);
+
+
+        if (maxBoostVelocity > maxDirectionalVelocity) thrustExtraForce = maxBoostVelocity-maxDirectionalVelocity;
+        else thrustExtraForce = 0;
+
+        if (maxBoostVelocity*2 > maxDirectionalVelocityX) thrustExtraForceX = maxBoostVelocity*2-maxDirectionalVelocityX;
+        else thrustExtraForceX = 0;
+        //print("dir: " + thrustExtraForceX);
+
+        int angle = (int)rigidBody.rotation.eulerAngles.z;
+        
+        angle = angle - (angle / 360) * 360;
+        if (angle < 0) angle = angle + 360;
+        var quadrant = (angle / 90) % 4;
+        //var positive = (quadrant == 3 || quadrant == 2);
+        //if (positive) print(angle);
+        
+        if (!(sameSign((quadrant==3||quadrant==2)?1:-1, rigidBody.GetRelativePointVelocity(Vector3.zero).x)) || (int)maxDirectionalVelocityX == 0)
+        {
+            thrustExtraForceX = maxBoostVelocity*2;
+            //if ((int)maxDirectionalVelocityX != 0) print("quad: " + quadrant); 
+        }
+
+
+        float forcePerFrame = (boostThrust * thrustExtraForce) * Time.deltaTime;
+        float forcePerFrameX = (boostThrust * thrustExtraForceX) * Time.deltaTime;
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.Space))
+        {
+            //print("up: " + forcePerFrame);
+            
+            rigidBody.AddForce((Vector3.up*forcePerFrame * Mathf.Cos(rigidBody.rotation.eulerAngles.z * 0.01745329251f)) + (Vector3.left * forcePerFrameX * Mathf.Sin(rigidBody.rotation.eulerAngles.z * 0.01745329251f)));
+            //rigidBody.
+            //rigidBody.transform.
+            if (!thrusterSound.isPlaying)
+            {
+                //print("euler: " + transform.rotation.eulerAngles.z);
+                thrusterSound.Play();
+            }
+ 
+        }
+        else
+        {
+            thrusterSound.Stop();
+        }
+    }
+
+    private void ResetRotation()
+    {
+        //if (Math.Abs(rigidBody.rotation.x) > 0.002)
+        //{
+            
+        //}
+    }
+    private void Rotate()
+    {
+        
+        float rotationPerFrame = Time.deltaTime * rcsThrust* (1+rcsExtraThrust);
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Rotate(Vector3.forward * Time.deltaTime*50);
+            transform.Rotate(Vector3.forward * rotationPerFrame );
+            rcsExtraThrust += rcsThrustIncrease * Time.deltaTime;
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            transform.Rotate(Vector3.back*Time.deltaTime*50);
+            transform.Rotate(Vector3.back * rotationPerFrame);
+            rcsExtraThrust += rcsThrustIncrease * Time.deltaTime;
         }
+        else
+        {
+            rcsExtraThrust = 0f;
+        }
+        
     }
 }
