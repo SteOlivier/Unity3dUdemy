fc95a5a7143993397e046a64e1613b7d351bb1e3
Stefan Olivier <stefan.olivier.car@gmail.com>
2021-01-21 01:53:28 +0200

    Added a little mood music, showtout to Sonic Pi team

---
+++ Assets/MusicPlayer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+    private AudioSource audioSource;
+    [SerializeField] int repeatTimes = 2;
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        //repeatTimes--;
+    }
+    void Update()
+    {
+        if (audioSource.isPlaying && repeatTimes <= 0)
+        {
+            audioSource.Stop();
+        }
+        else if (!audioSource.isPlaying && repeatTimes > 0)
+        {
+            audioSource.Play();
+            repeatTimes--;
+        }
+    }
+}
